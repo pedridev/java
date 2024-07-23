877b5057e9d5c8b8f59f5244ec324a830a88781f
pedri.dev <pedridevgh@gmail.com>
2024-07-23 16:22:07 -0300

    Initial Commit

--- src/EntradaSaidaDeDados.java
+++ src/EntradaSaidaDeDados.java
@@ -1,23 +1,23 @@
-//import java.util.Scanner;
-//
-//public class EntradaSaidaDeDados {
-//    public static void main(String[] args) {
-////        System.out.print("Eu faço a impressão e" + " pulo a linha");
-////        System.out.println("Eu faço a impressão" + " na mesma linha");
-////        System.out.printf("E eu faço a " + "impressão formatada");
-//
-//        Scanner scanner = new Scanner(System.in);
-//
-//        System.out.println("Olá, digite o seu nome : ");
-//        String nome = scanner.nextLine();
-//
-//        System.out.println("Nome : " + nome);
-//        System.out.println();
-//        System.out.println("Digite sua idade : ");
-//        int idade = scanner.nextInt();
-//        System.out.println("Idade : " + idade);
-//        System.out.println("Digite se você está empregado sim (true) ou não (false)");
-//        boolean empregado = scanner.nextBoolean();
-//        System.out.println("Olá, sou " + nome + " tenho " + idade + " anos e estou empregado " + empregado);
-//    }
-//}
+import java.util.Scanner;
+
+public class EntradaSaidaDeDados {
+    public static void main(String[] args) {
+//        System.out.print("Eu faço a impressão e" + " pulo a linha");
+//        System.out.println("Eu faço a impressão" + " na mesma linha");
+//        System.out.printf("E eu faço a " + "impressão formatada");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Olá, digite o seu nome : ");
+        String nome = scanner.nextLine();
+
+        System.out.println("Nome : " + nome);
+        System.out.println();
+        System.out.println("Digite sua idade : ");
+        int idade = scanner.nextInt();
+        System.out.println("Idade : " + idade);
+        System.out.println("Digite se você está empregado sim (true) ou não (false)");
+        boolean empregado = scanner.nextBoolean();
+        System.out.println("Olá, sou " + nome + " tenho " + idade + " anos e estou empregado " + empregado);
+    }
+}
--- src/EstudoArrayList.java
+++ src/EstudoArrayList.java
@@ -1,44 +1,44 @@
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//public class EstudoArrayList {
-//    public static void main(String[] args) {
-//
-//        List<String> listaDeCompras = new ArrayList<>();
-//
-//        listaDeCompras.add("Mamão");
-//        listaDeCompras.add("Sabão em Pó");
-//        listaDeCompras.add("Macarrão");
-//        listaDeCompras.add("Shampoo");
-//        listaDeCompras.add("Condicionador");
-//
-//
-//        listaDeCompras.add(1, "Banana");
-//
-//        for (String item2 : listaDeCompras) {
-//            System.out.println(item2);
-//        }
-//        // como localizar um item
-//
-//        String item = listaDeCompras.get(3);
-//        System.out.println("Na posição 3 eu tenho : " + item);
-//
-//        listaDeCompras.remove(0);
-//
-//        for (String item2 : listaDeCompras) {
-//            System.out.println(item2);
-//        }
-//
-//        boolean eVazia = listaDeCompras.isEmpty();
-//        int tamanho = listaDeCompras.size();
-//        boolean contem = listaDeCompras.contains("Arroz");
-//
-//        System.out.println("A lista esta vazia? : " + eVazia + " Qual o tamanho da lista? " + tamanho + ", a lista contem o item Arroz? " + contem);
-//
-////        listaDeCompras.clear();
-////        System.out.println("A lista esta vazia? " + listaDeCompras.isEmpty());
-//
-//        listaDeCompras.forEach(produto -> System.out.println(produto));
-//
-//    }
-//}
+import java.util.ArrayList;
+import java.util.List;
+
+public class EstudoArrayList {
+    public static void main(String[] args) {
+
+        List<String> listaDeCompras = new ArrayList<>();
+
+        listaDeCompras.add("Mamão");
+        listaDeCompras.add("Sabão em Pó");
+        listaDeCompras.add("Macarrão");
+        listaDeCompras.add("Shampoo");
+        listaDeCompras.add("Condicionador");
+
+
+        listaDeCompras.add(1, "Banana");
+
+        for (String item2 : listaDeCompras) {
+            System.out.println(item2);
+        }
+        // como localizar um item
+
+        String item = listaDeCompras.get(3);
+        System.out.println("Na posição 3 eu tenho : " + item);
+
+        listaDeCompras.remove(0);
+
+        for (String item2 : listaDeCompras) {
+            System.out.println(item2);
+        }
+
+        boolean eVazia = listaDeCompras.isEmpty();
+        int tamanho = listaDeCompras.size();
+        boolean contem = listaDeCompras.contains("Arroz");
+
+        System.out.println("A lista esta vazia? : " + eVazia + " Qual o tamanho da lista? " + tamanho + ", a lista contem o item Arroz? " + contem);
+
+//        listaDeCompras.clear();
+//        System.out.println("A lista esta vazia? " + listaDeCompras.isEmpty());
+
+        listaDeCompras.forEach(produto -> System.out.println(produto));
+
+    }
+}
